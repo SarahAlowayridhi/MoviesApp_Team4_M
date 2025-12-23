ff6026c5d9f9f9a58ca339cdc0988996f60d7826
sara <saraalialowayridhi@gmail.com>
2025-12-23 11:00:36 +0300

    Initial Commit

---
+++ MoviesApp_Team4_M/MoviesApp_Team4_MApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp_Team4_MApp.swift
+//  MoviesApp_Team4_M
+//
+//  Created by Sarah Alowayridhi on 03/07/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp_Team4_MApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
