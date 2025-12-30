94578625d8b09369a0a5936b2ceaa5a5a4d15b2d
sara <saraalialowayridhi@gmail.com>
2025-12-30 11:17:09 +0300

    SignIn API (GET USERS)

--- MoviesApp_Team4_M/Model/Users.swift
+++ MoviesApp_Team4_M/Model/Users.swift
@@ -5,3 +5,22 @@
 //  Created by Sarah Alowayridhi on 08/07/1447 AH.
 //
 
+import Foundation
+
+struct UsersResponse: Decodable {
+    let records: [UserRecord]
+}
+
+struct UserRecord: Decodable { //عندنا مجموعة من المستخدمين
+    let id: String
+    let fields: User
+}
+
+struct User: Decodable {
+    let email: String
+    let password: String
+}
+
+
+
+
---
+++ MoviesApp_Team4_M/ViewModel/SignInViewModel.swift
@@ -0,0 +1,50 @@
+//
+//  SignInViewModel.swift
+//  MoviesApp_Team4_M
+//
+//  Created by Sarah Alowayridhi on 10/07/1447 AH.
+//
+
+
+import Foundation
+import Combine
+
+class SignInViewModel: ObservableObject {
+
+    //  نخزن المستخدمين
+    @Published var users: [UserRecord] = []
+
+    //  نجيب المستخدمين من API
+    func getUsers() {
+
+        guard let url = URL(string: "https://api.airtable.com/v0/appsfcB6YESLj4NCN/users") else {
+            return
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+   
+        URLSession.shared.dataTask(with: request) { data, response, error in
+
+            guard let data = data else { return }
+
+            let decodedResponse = try? JSONDecoder().decode(UsersResponse.self, from: data)
+
+            DispatchQueue.main.async {
+                self.users = decodedResponse?.records ?? []
+            }
+
+        }.resume()
+    }
+
+    //  التحقق من تسجيل الدخول
+    func signIn(email: String, password: String) -> Bool {
+
+        let user = users.first { record in
+            record.fields.email == email &&
+            record.fields.password == password
+        }
+
+        return user != nil
+    }
+}
